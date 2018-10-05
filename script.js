8a494574524241e50e562b32a50dd3442ba29155
Param Thakkar <pht0104@gmail.com>
2018-10-05 12:41:50 -0400

    Optimize for mobile devices
     - add ability to click all buttons with enter key

--- script.js
+++ script.js
@@ -24,4 +24,33 @@ window.onload = function(){
   btn3.style.marginRight = window.innerWidth/50 + "px";
   btn4.style.marginLeft = window.innerWidth/50 + "px";
   btn4.style.marginRight = window.innerWidth/50 + "px";
+
+  if(screen.width<600){
+    btn1.style.width = window.innerWidth/3 + "px";
+    btn1.style.height = window.innerWidth/3 + "px";
+    btn2.style.width = window.innerWidth/3 + "px";
+    btn2.style.height = window.innerWidth/3 + "px";
+    btn3.style.width = window.innerWidth/3 + "px";
+    btn3.style.height = window.innerWidth/3 + "px";
+    btn4.style.width = window.innerWidth/3 + "px";
+    btn4.style.height = window.innerWidth/3 + "px";
+
+    btn1.style.backgroundColor = "inherit";
+    btn1.style.color = "white";
+    btn2.style.backgroundColor = "inherit";
+    btn2.style.color = "white";
+    btn3.style.backgroundColor = "inherit";
+    btn3.style.color = "white";
+    btn4.style.backgroundColor = "inherit";
+    btn4.style.color = "white";
+
+    btn1.style.marginBottom = window.innerWidth/50 + "px";
+    btn1.style.marginRight = window.innerWidth/30 + "px";
+    btn2.style.marginLeft = window.innerWidth/30 + "px";
+    btn2.style.marginBottom = window.innerWidth/50 + "px";
+    btn3.style.marginTop = window.innerWidth/50 + "px";
+    btn3.style.marginRight = window.innerWidth/30 + "px";
+    btn4.style.marginLeft = window.innerWidth/30 + "px";
+    btn4.style.marginTop = window.innerWidth/50 + "px";
+  }
 };
